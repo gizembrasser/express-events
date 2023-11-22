3b66285fc205ec31efb4da77233eb736b596119b
gizembrasser <gizembrasser101@gmail.com>
2023-11-22 11:56:00 +0100

    Added username filter

--- src/services/users/getUsers.js
+++ src/services/users/getUsers.js
@@ -1,10 +1,14 @@
 import userData from "../../data/users.json" assert { type: "json" };
 
-const getUsers = (name) => {
+const getUsers = (name, username) => {
     let users = userData.users;
 
     if (name) {
         users = users.filter(user => user.name === name);
+    }
+
+    if (username !== undefined) {
+        users = users.filter(user => user.username === JSON.parse(username));
     }
 
     return users;
