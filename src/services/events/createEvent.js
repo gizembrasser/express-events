2824ba602d0c8d2035129f5f140dd8afb7337920
gizembrasser <gizembrasser101@gmail.com>
2023-11-22 13:52:51 +0100

    Added parameters

--- src/services/events/createEvent.js
+++ src/services/events/createEvent.js
@@ -1,14 +1,17 @@
 import eventData from "../..data/events.json" assert { type: "json" };
 import { v4 as uuid } from "uuid";
 
-const createEvent = (createdBy, title, description, image, categoryIds) => {
+const createEvent = (createdBy, title, description, image, categoryIds, location, startTime, endTime) => {
     const newEvent = {
         id: uuid(),
         createdBy,
         title,
         description,
         image,
-        categoryIds
+        categoryIds,
+        location,
+        startTime,
+        endTime
     };
 
     eventData.events.push(newEvent);
