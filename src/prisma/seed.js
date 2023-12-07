9056ab229d422a7456c3c297575a384a2d40ece1
gizembrasser <gizembrasser101@gmail.com>
2023-12-07 13:22:20 +0100

    add: events for loop

--- src/prisma/seed.js
+++ src/prisma/seed.js
@@ -8,21 +8,44 @@ const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
 async function main() {
     const { users } = userData;
     const { categories } = categoryData;
+    const { events } = eventData;
 
     for (const user of users) {
         await prisma.user.upsert({
             where: { id: user.id },
             update: {},
             create: user
-        })
-    };
+        });
+    }
     for (const category of categories) {
         await prisma.category.upsert({
             where: { id: category.id },
             update: {},
             create: category
-        })
-    };
+        });
+    }
+    for (const event of events) {
+        await prisma.event.upsert({
+            where: { id: event.id },
+            update: {},
+            create: {
+                id: event.id,
+                title: event.title,
+                description: event.description,
+                image: event.image,
+                categoryIds: {
+                    // connect creates a many-to-many relationship between Categories and Events.
+                    connect: event.categoryIds.map((id) => ({ id }))
+                },
+                location: event.location,
+                startTime: event.startTime,
+                endTime: event.endTime,
+                user: {
+                    connect: { id: event.createdBy }
+                }
+            }
+        });
+    }
 };
 
 main()
