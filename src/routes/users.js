d8b52a831003e38a418c5f0ee9c1e9f01fe83305
gizembrasser <gizembrasser101@gmail.com>
2023-12-10 14:49:13 +0100

    update: async...await on route handlers

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,38 +9,60 @@ import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    const { name } = req.query;
-    const users = getUsers(name);
-    res.status(200).json(users);
+router.get("/", async (req, res, next) => {
+    try {
+        const users = await getUsers();
+        res.json(users);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.post("/", (req, res) => {
-    const { username, password, name, image } = req.body;
-    const newUser = createUser(username, password, name, image);
-    res.status(201).json(newUser);
+router.post("/", async (req, res, next) => {
+    try {
+        const { username, password, name, image } = req.body;
+        const newUser = await createUser(username, password, name, image);
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.get("/:id", (req, res) => {
-    const { id } = req.params;
-    const user = getUserById(id);
-    res.status(200).json(user);
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await getUserById(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 }, notFoundErrorHandler);
 
-router.put("/:id", authMiddleware, (req, res) => {
-    const { id } = req.params;
-    const { username, password, name, image } = req.body;
-    const updatedUser = updateUserById(id, username, password, name, image);
-    res.status(200).json(updatedUser);
+router.put("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { username, password, name, image } = req.body;
+        const updatedUser = await updateUserById(id, username, password, name, image);
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
 }, notFoundErrorHandler);
 
-router.delete("/:id", authMiddleware, (req, res) => {
-    const { id } = req.params;
-    const deletedUserId = deleteUser(id);
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedUserId = await deleteUser(id);
 
-    res.status(200).json({
-        message: `User with id ${deletedUserId} was deleted.`
-    });
+        res.status(200).json({
+            message: `User with id ${deletedUserId} was deleted.`
+        });
+    } catch (error) {
+        next(error);
+    }
 }, notFoundErrorHandler);
 
 export default router;
